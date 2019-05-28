a40627f74346cd89294031ed3652faba283e68c3
Aaron Targa <33754619+AaronTarga@users.noreply.github.com>
2019-05-28 17:05:10 +0200

    removed echo

--- download.sh
+++ download.sh
@@ -1,7 +1,6 @@
 
 link=$1
 name=$(wget -nv $link 2>&1 | cut -d\" -f2)
-echo $name
 name=$(echo "$name" | sed 's/ /\\ /g')
 name=$(echo "$name" | sed 's/)/\\)/g')
 name=$(echo "$name" | sed 's/(/\\(/g')
